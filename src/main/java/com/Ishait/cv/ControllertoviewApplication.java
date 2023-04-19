b405f4e3c6d991912300ab7f66b62d5c75d6fd99
Isha <isharathore345@gmail.com>
2023-04-19 23:18:55 +0530

    first commit

---
+++ src/main/java/com/Ishait/cv/ControllertoviewApplication.java
@@ -0,0 +1,13 @@
+package com.Ishait.cv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControllertoviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControllertoviewApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Ishait/cv/ControllertoviewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Ishait.cv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControllertoviewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
